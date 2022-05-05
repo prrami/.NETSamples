4d9308c0d4fde51b011b3b350ce278b3b295511d
Karan Rami <karanramiit@gmail.com>
2022-05-06 00:46:11 +0530

    SeriLog & WebApiDemo

---
+++ SeriLogSample/SeriLogSample/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace SeriLogSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                configureSeriLog(Guid.NewGuid().ToString());
+                Trace.TraceInformation("Hello World! This is serilog test");
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+        }
+        static void configureSeriLog(string CorrelationID)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(getExecutingDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var listener = new global::SerilogTraceListener.SerilogTraceListener();
+            Trace.Listeners.Clear();
+            Trace.Listeners.Add(listener);
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("CorrelationID", CorrelationID)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+        private static string getExecutingDirectory()
+        {
+            var location = Assembly.GetExecutingAssembly().Location;
+            return Path.GetDirectoryName(location);
+        }
+    }
+}
