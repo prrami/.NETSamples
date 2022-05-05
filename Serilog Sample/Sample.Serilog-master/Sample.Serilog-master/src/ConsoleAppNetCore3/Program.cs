c12462e454ea6e113555feb2a0ab9ef981523708
Karan Rami <karanramiit@gmail.com>
2022-05-06 00:30:16 +0530

    SerilogSample

---
+++ Serilog Sample/Sample.Serilog-master/Sample.Serilog-master/src/ConsoleAppNetCore3/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+
+namespace ConsoleAppNetCore3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SetupStaticLogger();
+
+			try
+			{
+				RunApp();
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "An unhandled exception occurred.");
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+		}
+
+		private static void SetupStaticLogger()
+		{
+			var configuration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			Log.Logger = new LoggerConfiguration()
+				.ReadFrom.Configuration(configuration)
+				.CreateLogger();
+		}
+
+		private static void RunApp()
+		{
+			// Do not pass any logger in via Dependency Injection, as the class will simply reference the static logger.
+			var classThatLogs = new ClassThatLogs();
+			classThatLogs.WriteLogs();
+		}
+	}
+}
